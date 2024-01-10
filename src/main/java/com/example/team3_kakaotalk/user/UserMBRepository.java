405ebb8a615c6043e4019b47105b0f656cb22ac3
seunggod <kiss0703@nate.com>
2024-01-10 10:17:48 +0900

    commit

--- src/main/java/com/example/team3_kakaotalk/user/UserMBRepository.java
+++ src/main/java/com/example/team3_kakaotalk/user/UserMBRepository.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMBRepository {
 	
+	// 친구 탭 메인
 	public List<UserResponse.FriendTepMainResponseDTO> findByFriendTepMain(Integer id);
 	
 	// 나의 프로필 수정 및 삭제
@@ -15,8 +16,15 @@ public interface UserMBRepository {
 
 	// 나의 프로필 상세보기
 	public UserResponse.MyProfileDetailResponseDTO findByMyProfileDetail(Integer id);
+	
 	// 친구 프로필 상세보기
 	public UserResponse.FriendProfileDetailResponseDTO findByFriendProfileDetail(Integer id);
+	
+	// 연락처로 친구 추가
+	public void phoneNumFriendAdd();
+
+	// 이메일로 친구 추가
+	public void emailFriendAdd();
 
 
 }
