4bdeacedd6f4a04b96aae8fdeb6f2a5bd5de3d9e
heidi505 <xxwhite19@gmail.com>
2024-01-16 16:14:03 +0900

    프로필 ì머지 오류

--- src/main/java/com/example/team3_kakaotalk/user/UserRestController.java
+++ src/main/java/com/example/team3_kakaotalk/user/UserRestController.java
@@ -61,33 +61,33 @@ public class UserRestController {
     }
 
     // 나의 프로필 수정
-    @PostMapping("/my-profile-update")
-    public ResponseEntity<?> myProfileUpdate(@RequestBody UserRequest.MyProfileUpdateRequestDTO myProfileUpdateRequestDto) {
-        this.userService.myProfileUpdate(myProfileUpdateRequestDto);
-        return ResponseEntity.ok().body(ApiUtils.success(null));
-    }
-
-    // 나의 프로필 삭제(프로필 이미지)
-    @GetMapping("/my-profileImage-delete/{id}")
-    public ResponseEntity<?> myProfileImageDelete(@PathVariable Integer id) {
-        this.userService.myProfileImageDelete(id);
-        return ResponseEntity.ok().body(ApiUtils.success(null));
-    }
-
-    // 나의 프로필 삭제(배경 이미지)
-    @GetMapping("/my-profileBackImage-delete/{id}")
-    public ResponseEntity<?> myProfileBackImageDelete(@PathVariable Integer id) {
-        this.userService.myProfileBackImageDelete(id);
-        return ResponseEntity.ok().body(ApiUtils.success(null));
-    }
-
-
-	// 나의 프로필 수정
-	@PostMapping("/my-profile-update")
-	public ResponseEntity<?> myProfileUpdate(@RequestBody UserRequest.MyProfileUpdateRequestDTO myProfileUpdateRequestDto){
-		this.userService.myProfileUpdate(myProfileUpdateRequestDto);
-		return ResponseEntity.ok().body(ApiUtils.success(null));
-	}
+//    @PostMapping("/my-profile-update")
+//    public ResponseEntity<?> myProfileUpdate(@RequestBody UserRequest.MyProfileUpdateRequestDTO myProfileUpdateRequestDto) {
+//        this.userService.myProfileUpdate(myProfileUpdateRequestDto);
+//        return ResponseEntity.ok().body(ApiUtils.success(null));
+//    }
+//
+//    // 나의 프로필 삭제(프로필 이미지)
+//    @GetMapping("/my-profileImage-delete/{id}")
+//    public ResponseEntity<?> myProfileImageDelete(@PathVariable Integer id) {
+//        this.userService.myProfileImageDelete(id);
+//        return ResponseEntity.ok().body(ApiUtils.success(null));
+//    }
+//
+//    // 나의 프로필 삭제(배경 이미지)
+//    @GetMapping("/my-profileBackImage-delete/{id}")
+//    public ResponseEntity<?> myProfileBackImageDelete(@PathVariable Integer id) {
+//        this.userService.myProfileBackImageDelete(id);
+//        return ResponseEntity.ok().body(ApiUtils.success(null));
+//    }
+//
+//
+//	// 나의 프로필 수정
+//	@PostMapping("/my-profile-update")
+//	public ResponseEntity<?> myProfileUpdate(@RequestBody UserRequest.MyProfileUpdateRequestDTO myProfileUpdateRequestDto){
+//		this.userService.myProfileUpdate(myProfileUpdateRequestDto);
+//		return ResponseEntity.ok().body(ApiUtils.success(null));
+//	}
 
 	// 나의 프로필 삭제(프로필 이미지)
 	@GetMapping("/my-profileImage-delete/{id}")
