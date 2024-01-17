fe2adf6126e230f1bf77435092c68d060f8250ef
namgooo <namgo9204@naver.com>
2024-01-17 17:14:27 +0900

    commit

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -10,18 +10,13 @@ insert into user_tb(`email`,`nickname`,`password`, `phone_num`, `birthdate`, `cr
 insert into user_tb(`email`,`nickname`,`password`, `phone_num`, `birthdate`, `created_at`, `profile_id`)values('red@nate.com','학원가고싶다','1111','01015208950','2024-01-16', now(), 10);
 
 
-insert into friend_tb(`user_id1`, `user_id2`) values ('1','3');
-insert into friend_tb(`user_id1`, `user_id2`) values ('1','2');
-insert into friend_tb(`user_id1`, `user_id2`) values ('1','4');
-insert into friend_tb(`user_id1`, `user_id2`) values ('1','5');
-insert into friend_tb(`user_id1`, `user_id2`) values ('1','9');
-insert into friend_tb(`user_id1`, `user_id2`) values ('1','10');
-
 
 insert into friend_tb(`user_id1`, `user_id2`,`is_favorite`) values ('1','3',true);
 insert into friend_tb(`user_id1`, `user_id2`,`is_favorite`) values ('1','2',true);
 insert into friend_tb(`user_id1`, `user_id2`,`is_favorite`) values ('1','4',false);
 insert into friend_tb(`user_id1`, `user_id2`,`is_favorite`) values ('1','5',false);
+insert into friend_tb(`user_id1`, `user_id2`,`is_favorite`) values ('1','9',false);
+insert into friend_tb(`user_id1`, `user_id2`,`is_favorite`) values ('1','10',false);
 
 
 insert into profile_tb(user_id, profile_image, status_message, back_image, qr_code)
