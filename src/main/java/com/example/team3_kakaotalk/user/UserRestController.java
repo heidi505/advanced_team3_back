3ad04310b30fb079ddb7ae495fca41cccffe3b59
YHYELIM <yhl0513@naver.com>
2024-01-17 09:05:37 +0900

    커밋

--- src/main/java/com/example/team3_kakaotalk/user/UserRestController.java
+++ src/main/java/com/example/team3_kakaotalk/user/UserRestController.java
@@ -83,25 +83,25 @@ public class UserRestController {
 
 
 	// 나의 프로필 수정
-	@PostMapping("/my-profile-update")
-	public ResponseEntity<?> myProfileUpdate(@RequestBody UserRequest.MyProfileUpdateRequestDTO myProfileUpdateRequestDto){
-		this.userService.myProfileUpdate(myProfileUpdateRequestDto);
-		return ResponseEntity.ok().body(ApiUtils.success(null));
-	}
+//	@PostMapping("/my-profile-update")
+//	public ResponseEntity<?> myProfileUpdate(@RequestBody UserRequest.MyProfileUpdateRequestDTO myProfileUpdateRequestDto){
+//		this.userService.myProfileUpdate(myProfileUpdateRequestDto);
+//		return ResponseEntity.ok().body(ApiUtils.success(null));
+//	}
 
 	// 나의 프로필 삭제(프로필 이미지)
-	@GetMapping("/my-profileImage-delete/{id}")
-	public ResponseEntity<?> myProfileImageDelete(@PathVariable Integer id){
-		this.userService.myProfileImageDelete(id);
-		return ResponseEntity.ok().body(ApiUtils.success(null));
-	}
+//	@GetMapping("/my-profileImage-delete/{id}")
+//	public ResponseEntity<?> myProfileImageDelete(@PathVariable Integer id){
+//		this.userService.myProfileImageDelete(id);
+//		return ResponseEntity.ok().body(ApiUtils.success(null));
+//	}
 
 	// 나의 프로필 삭제(배경 이미지)
-	@GetMapping("/my-profileBackImage-delete/{id}")
-	public ResponseEntity<?> myProfileBackImageDelete(@PathVariable Integer id){
-		this.userService.myProfileBackImageDelete(id);
-		return ResponseEntity.ok().body(ApiUtils.success(null));
-	}
+//	@GetMapping("/my-profileBackImage-delete/{id}")
+//	public ResponseEntity<?> myProfileBackImageDelete(@PathVariable Integer id){
+//		this.userService.myProfileBackImageDelete(id);
+//		return ResponseEntity.ok().body(ApiUtils.success(null));
+//	}
 
 	// 친구 차단
 	@GetMapping("/friend-delete/{id}")
